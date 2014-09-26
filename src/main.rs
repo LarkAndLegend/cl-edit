f34974bcc96d275585d24d01d7680db3e5406726
Mark E. McDermott <mark.elias.mcdermott@gmail.com>
2014-09-26 12:05:52 -0400

    Enabled GL

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate native;
 extern crate glfw;
+extern crate gl;
 
 use glfw::Context;
 
@@ -31,9 +32,19 @@ fn main() {
         }
     });
 
+
     // open a window
+    glfw.window_hint(glfw::ContextVersion(3,2));
+    glfw.window_hint(glfw::OpenglForwardCompat(true));
+    glfw.window_hint(glfw::OpenglProfile(glfw::OpenGlCoreProfile));
+
     let (window, events) = glfw.create_window(300,300,"Cosmolark Asset Editor",glfw::Windowed)
                                .expect("Failed to open window.");
+
+    // set up GL 
+    gl::load_with(|s| window.get_proc_address(s));
+    gl::ClearColor(0.3,0.3,0.3,1.0);
+    glfw.set_swap_interval(0); // Turn off vertical sync
 
     window.set_cursor_pos_polling(true);
     window.set_key_polling(true);
@@ -45,11 +56,25 @@ fn main() {
     window.make_current();
     window.set_cursor_mode(glfw::CursorNormal);
 
+    let mut frame_cnt: u64 = 0;
+    let mut prev_time: f64 = glfw.get_time();
+
     while !window.should_close() {
         glfw.poll_events();
         for (_,event) in glfw::flush_messages(&events) {
             handle_window_event(&window, event);
         }
+
+        // count frames
+        frame_cnt += 1;
+        if 0 == frame_cnt % 100 {
+            let curr_time = glfw.get_time();
+            println!("Frames Per Second = {}", 100.0/(curr_time-prev_time));
+            prev_time = curr_time;
+        }
+
+        gl::Clear(gl::COLOR_BUFFER_BIT);
+        window.swap_buffers();
     }
 } // end function main()
 
