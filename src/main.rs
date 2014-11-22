b36da95d057f854fc3a75862bf6fa11566ac842e
Mark E. McDermott <mark.elias.mcdermott@gmail.com>
2014-11-22 04:47:05 -0500

    basic SDL2 window working

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,12 @@
 // ---------------------------------------------------------------------------
 // ---------------------------------------------------------------------------
 
-
+extern crate sdl2;
 extern crate native;
-extern crate glfw;
-extern crate gl;
 
-use glfw::Context;
+
+use sdl2::video::{Window, PosCentered, OPENGL};
+use sdl2::event::{Quit, None, poll_event};
 
 // --------------------------------------------------------------------------
 // Native entry point
@@ -32,115 +32,50 @@ fn start(argc: int, argv: *const *const u8) -> int {
 fn main() {
     println!("Welcome to the the Cosmolark editor!")
     
-    let glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
-
-    // print out available modes
-    glfw.with_primary_monitor(|monitor| {
-        let _ = monitor.map(|monitor| {
-            println!("{}:", monitor.get_name());
-            println!("    {}\n",monitor.get_video_mode().unwrap());
-        });
-    });
-
-    println!("Available Monitors\n------------------");
-    glfw.with_connected_monitors(|monitors| {
-        for monitor in monitors.iter() {
-            println!("{}:", monitor.get_name());
-            for mode in monitor.get_video_modes().iter() {
-                println!("    {}",*mode);
-            }
-        }
-    });
+    sdl2::init(sdl2::INIT_EVERYTHING);
 
 
     // open a window
-    glfw.window_hint(glfw::ContextVersion(3,2));
-    glfw.window_hint(glfw::OpenglForwardCompat(true));
-    glfw.window_hint(glfw::OpenglProfile(glfw::OpenGlCoreProfile));
+    let window = match Window::new("Cosmolark Asset Editor",PosCentered, PosCentered, 300,300,OPENGL) {
+        Ok(window) => window,
+        Err(err)   => panic!("failed to create window: {}", err)
+    };
+    window.show();
+    
+    // create a rendering context
+    let renderer = match sdl2::render::Renderer::from_window(window, sdl2::render::RenderDriverIndex::Auto, sdl2::render::ACCELERATED) {
+        Ok(renderer) => renderer,
+        Err(err)     => panic!("failed to create renderer: {}", err)
+    };
+    
+    let _ = renderer.set_draw_color(sdl2::pixels::RGB(101,208,246)); // light blue
+    let _ = renderer.clear();
+    
+    let _ = renderer.set_draw_color(sdl2::pixels::RGB(0,153,204)); // dark blue
+    let border_rect = sdl2::rect::Rect::new(320-64, 240-64, 128, 128);
+    let _ = match renderer.draw_rect(&border_rect) {
+        Ok(_) => {},
+        Err(err) => panic!("failed to draw rect: {}", err)
+    };
+    
+    // swap buffer
+    let _ = renderer.present();
+    
 
-    let (window, events) = glfw.create_window(300,300,"Cosmolark Asset Editor",glfw::Windowed)
-                               .expect("Failed to open window.");
-
-    // set up GL 
-    gl::load_with(|s| window.get_proc_address(s));
-    gl::ClearColor(0.3,0.3,0.3,1.0);
-    glfw.set_swap_interval(0); // Turn off vertical sync
-
-    window.set_cursor_pos_polling(true);
-    window.set_key_polling(true);
-    window.set_cursor_enter_polling(true);
-    window.set_size_polling(true);
-    window.set_char_polling(true);
-    window.set_mouse_button_polling(true);
-
-    window.make_current();
-    window.set_cursor_mode(glfw::CursorNormal);
-
-    let mut frame_cnt: u64 = 0;
-    let mut prev_time: f64 = glfw.get_time();
-
-    while !window.should_close() {
-        glfw.poll_events();
-        for (_,event) in glfw::flush_messages(&events) {
-            handle_window_event(&window, event);
+    'event : loop {
+        match poll_event() {
+            Quit(_) => break 'event,
+            None    => continue,
+            event   => println!("event: {}", event),
         }
-
-        // count frames
-        frame_cnt += 1;
-        if 0 == frame_cnt % 100 {
-            let curr_time = glfw.get_time();
-            println!("Frames Per Second = {}", 100.0/(curr_time-prev_time));
-            prev_time = curr_time;
-        }
-
-        
-        gl::Clear(gl::COLOR_BUFFER_BIT);
-        window.swap_buffers();
     }
+    
+    sdl2::quit();
 } // end function main()
 
 
 
-// --------------------------------------------------------------------------
-// Monolithic event handler function. TODO: refactor into proper event 
-// handling mechanism.
-// --------------------------------------------------------------------------
-fn handle_window_event(window: &glfw::Window, event: glfw::WindowEvent) {
-    match event {
-        glfw::KeyEvent(glfw::KeyEscape, _, glfw::Press, _) => {
-            window.set_should_close(true)
-        },
-        glfw::CursorPosEvent(x,y) => {
-            println!("Cursor position ({},{})",x,y)
-        },
-        glfw::CursorEnterEvent(entered) => {
-            match entered {
-                true => println!("Cursor entered!"),
-                _    => println!("Cursor exited."),
-            }
-        }
-        glfw::SizeEvent(x,y) => {
-            println!("New size {}x{}",x,y)
-        },
-        glfw::MouseButtonEvent(button,action,_) => {
-           
-            let button_ix = match button {
-                glfw::MouseButton1 => 1u,
-                glfw::MouseButton2 => 2u,
-                glfw::MouseButton3 => 3u,
-                _                  => 0u,
-            };
 
-            if 0 < button_ix {
-                println!("Mouse event button {} {}",button_ix,action)
-            }
-        },
-        glfw::CharEvent(c) => {
-            println!("{}",c)
-        },
-        _ => {},
-    }
-} // end funtion handle_window_event()
 
 
 
